b8889537caa4c4710ab3f84b30f5bb566f5939e9
azu <azuciao@gmail.com>
2019-02-05 19:41:27 +0900

    fix(bookmark-fetch) mkdir

--- src/bookmark-fetch.ts
+++ src/bookmark-fetch.ts
@@ -5,12 +5,12 @@ import { parseMyData, ParsedResults } from "./parse-mydata";
 require("cross-fetch/polyfill");
 const CACHE_DIR = path.join(__dirname, "../.cache");
 const OUTPUT_PATH = path.join(CACHE_DIR, "search.data");
+
 export interface FetchHatenaBookmarksOptions {
     reload: boolean;
 }
 
 export const fetchHatenaBookmarks = async (userName: string, options: FetchHatenaBookmarksOptions): Promise<ParsedResults> => {
-    fs.mkdirSync(CACHE_DIR);
     const searchDataURL = `http://b.hatena.ne.jp/${encodeURIComponent(userName)}/search.data`;
     if (!options.reload && fs.existsSync(OUTPUT_PATH)) {
         const searchData = fs.readFileSync(OUTPUT_PATH, "utf-8");
@@ -24,6 +24,9 @@ export const fetchHatenaBookmarks = async (userName: string, options: FetchHaten
     }
     const text = await response.text();
     console.info(`Complete fetch: ${searchDataURL}`);
+    if (!fs.existsSync(CACHE_DIR)) {
+        fs.mkdirSync(CACHE_DIR);
+    }
     fs.writeFileSync(OUTPUT_PATH, text, "utf-8");
     return parseMyData(text);
 };
